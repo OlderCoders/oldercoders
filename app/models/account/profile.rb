e60680d698dea1002dde4296a2ba8d1752f59e04
Michael Bester <michael@kimili.com>
2019-04-16 10:21:15 -0400

    Some more profile validation tests.

--- app/models/account/profile.rb
+++ app/models/account/profile.rb
@@ -4,6 +4,7 @@ class Account::Profile < ApplicationRecord
   end
 
   before_save :sanitize_inputs
+  before_validation :verify_twitter_username, :verify_github_username
 
   belongs_to :account, inverse_of: :profile, foreign_key: 'account_id', touch: true
 
@@ -12,41 +13,47 @@ class Account::Profile < ApplicationRecord
   validates :birthday, presence: true
   validates :location, length: { maximum: 128 }
   validates :bio, length: { maximum: 255 }
-  validates :twitter_username, uniqueness: { allow_nil: true }, if: :twitter_username_changed?
-  validates :github_username, uniqueness: { allow_nil: true }, if: :github_username_changed?
+  validates :twitter_username,
+            length: { maximum: 128 },
+            uniqueness: { allow_nil: true },
+            if: :twitter_username_changed?
+  validates :github_username,
+            length: { maximum: 128 },
+            uniqueness: { allow_nil: true },
+            if: :github_username_changed?
   validates :website_url, :employer_url,
             length: { maximum: 128 },
             url: { allow_blank: true, no_local: true, schemes: %w[https http] }
   validates :facebook_url,
-            format: /\A(http(s)?:\/\/)?(www.facebook.com|facebook.com)\/.*\Z/,
+            format: /\A(http(s)?:\/\/)?(www.facebook.com|facebook.com)\/.+\Z/,
             length: { maximum: 128 },
             allow_blank: true
   validates :stackoverflow_url,
             allow_blank: true,
             length: { maximum: 128 },
             format:
-            /\A(http(s)?:\/\/)?(www.stackoverflow.com|stackoverflow.com|www.stackexchange.com|stackexchange.com)\/.*\Z/
+            /\A(http(s)?:\/\/)?(www.stackoverflow.com|stackoverflow.com|www.stackexchange.com|stackexchange.com)\/.+\Z/
   validates :behance_url,
             allow_blank: true,
             length: { maximum: 128 },
-            format: /\A(http(s)?:\/\/)?(www.behance.net|behance.net)\/.*\Z/
+            format: /\A(http(s)?:\/\/)?(www.behance.net|behance.net)\/.+\Z/
   validates :linkedin_url,
             allow_blank: true,
             length: { maximum: 128 },
             format:
-              /\A(http(s)?:\/\/)?(www.linkedin.com|linkedin.com|[A-Za-z]{2}.linkedin.com)\/.*\Z/
+              /\A(http(s)?:\/\/)?(www.linkedin.com|linkedin.com|[A-Za-z]{2}.linkedin.com)\/.+\Z/
   validates :dribbble_url,
             allow_blank: true,
             length: { maximum: 128 },
-            format: /\A(http(s)?:\/\/)?(www.dribbble.com|dribbble.com)\/.*\Z/
+            format: /\A(http(s)?:\/\/)?(www.dribbble.com|dribbble.com)\/.+\Z/
   validates :medium_url,
             allow_blank: true,
             length: { maximum: 128 },
-            format: /\A(http(s)?:\/\/)?(www.medium.com|medium.com)\/.*\Z/
+            format: /\A(http(s)?:\/\/)?(www.medium.com|medium.com)\/.+\Z/
   validates :gitlab_url,
             allow_blank: true,
             length: { maximum: 128 },
-            format: /\A(http(s)?:\/\/)?(www.gitlab.com|gitlab.com)\/.*\Z/
+            format: /\A(http(s)?:\/\/)?(www.gitlab.com|gitlab.com)\/.+\Z/
 
   private
 
@@ -54,6 +61,14 @@ class Account::Profile < ApplicationRecord
       return if birthday.blank?
 
       errors.add(:birthday, 'is too recent') if birthday > Time.zone.today - 21.years
-      errors.add(:birthday, 'is too long ago. You can\'t be that old.') if birthday < Time.zone.today - 121.years
+      errors.add(:birthday, 'is too long ago. We\'re old, but come on.') if birthday < Time.zone.today - 121.years
+    end
+
+    def verify_twitter_username
+      self.twitter_username = nil if twitter_username.blank?
+    end
+
+    def verify_github_username
+      self.github_username = nil if github_username.blank?
     end
 end
--- test/models/account/profile_test.rb
+++ test/models/account/profile_test.rb
@@ -4,9 +4,7 @@ class Account::ProfileTest < ActiveSupport::TestCase
 
   def setup
     @profile = account_profiles(:michael)
-    @url_props = %i[
-      website_url
-      employer_url
+    @social_url_props = %i[
       facebook_url
       linkedin_url
       stackoverflow_url
@@ -15,31 +13,120 @@ class Account::ProfileTest < ActiveSupport::TestCase
       behance_url
       gitlab_url
     ]
+    @other_url_props = %i[
+      website_url
+      employer_url
+    ]
+    @all_url_props = (@social_url_props & @other_url_props).uniq
+    @tlds = {
+      behance: 'net'
+    }
   end
 
   test "profile should be vaild" do
     assert @profile.valid?
   end
 
-  test "website urls should not be too long" do
-    @url_props.each do |url|
-      @profile.send "#{url}=", 'a' * 129
-      assert_not @profile.valid?
+  test "Birthday should be required" do
+    @profile.birthday = nil
+    assert_not @profile.valid?
+  end
+
+  test "Birthday should not be in the future" do
+    @profile.birthday = 1.day.from_now
+    assert_not @profile.valid?
+  end
+
+  test "Birthday should be within an older coder date range" do
+    @profile.birthday = 122.years.ago
+    assert_not @profile.valid?
+    @profile.birthday = 20.years.ago
+    assert_not @profile.valid?
+  end
+
+  test "website urls can be blank" do
+    @all_url_props.each do |url|
+      @profile.send "#{url}=", '   '
+      assert @profile.valid?
       @profile.restore_attributes
     end
   end
 
-  test "website should a valid URL" do
-    @url_props.each do |url|
+  test "website urls should have an https? scheme" do
+    @all_url_props.each do |url|
+      domain = valid_domain_for url
+      @profile.send "#{url}=", "#{domain}/#{@profile.account.username}"
+      assert_not @profile.valid?, "#{url.to_sym} should require a scheme"
+      %w[ftp file mailto ldap news tel telnet urn].each do |scheme|
+        @profile.send "#{url}=", "#{scheme}://#{domain}/#{@profile.account.username}"
+        assert_not @profile.valid?, "#{url.to_sym} incorrectly allowed scheme #{scheme}"
+      end
+      @profile.send "#{url}=", "http://#{domain}/#{@profile.account.username}"
+      assert @profile.valid?
+      @profile.send "#{url}=", "https://#{domain}/#{@profile.account.username}"
+      assert @profile.valid?
+      @profile.restore_attributes
+    end
+  end
+
+  test "social website urls should validate to their specific domains, and have at least a subdirectory" do
+    @social_url_props.each do |url|
+      domain = valid_domain_for url
+      @profile.send "#{url}=", "http://foo.com/"
+      assert_not @profile.valid?, "#{url.to_sym} validation allowed a domain it shouldn't have"
+      @profile.send "#{url}=", "http://#{domain}/"
+      assert_not @profile.valid?, "#{url.to_sym} validation allowed no URL segments"
+      @profile.send "#{url}=", "http://#{domain}/#{@profile.account.username}"
+      assert @profile.valid?, "#{url.to_sym} should allow URL segments"
+      @profile.restore_attributes
+    end
+  end
+
+  test "website urls should not be too long" do
+    @all_url_props.each do |url|
+      domain = valid_domain_for url
+      @profile.send "#{url}=", "http://#{domain}/#{'a' * 118}"
+      assert_not @profile.valid?, "#{url.to_sym} failed length validation being too long"
+      @profile.restore_attributes
+    end
+  end
+
+  test "website should reject invalid URLs" do
+    @all_url_props.each do |url|
       @profile.send "#{url}=", 'not a valid URL'
       assert_not @profile.valid?
       @profile.restore_attributes
     end
   end
 
-  test "location should not be too long" do
-    @profile.location = "a" * 129
+  test "location can be blank" do
+    @profile.location = nil
+    assert @profile.valid?
+  end
+
+  test "twitter can be blank" do
+    @profile.twitter_username = '   '
+    assert @profile.valid?
+  end
+
+  test "twitter username should not be too long" do
+    @profile.twitter_username = "a" * 129
     assert_not @profile.valid?
+  end
+
+  test "github username can be blank" do
+    @profile.github_username = '   '
+    assert @profile.valid?
+  end
+
+  test "github username should not be too long" do
+    @profile.github_username = "a" * 129
+    assert_not @profile.valid?
+  end
+
+  test "bio can be blank" do
+    @profile.bio = nil
+    assert @profile.valid?
   end
 
   test "bio should not be too long" do
@@ -67,5 +154,15 @@ class Account::ProfileTest < ActiveSupport::TestCase
       end
     end
   end
+
+  private
+
+    def valid_domain_for(sym)
+      domain = sym.to_s.gsub '_url', ''
+      tld = @tlds[domain.to_sym]
+      return "#{domain}.#{tld}" if tld.present?
+
+      "#{domain}.com"
+    end
 
 end
