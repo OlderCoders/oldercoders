b6a209f8ea0b56f74ad17a6c21fe55486e421732
Michael Bester <michael@kimili.com>
2019-04-17 05:42:44 -0400

    Adding PublicSans Typeface

--- frontend/styles/index.js
+++ frontend/styles/index.js
@@ -1,5 +1,9 @@
 // These are the base styles for the site.
 // Component specific styles should be defined their specific component files.
 
+// Global
+import "./fonts.css";
 import "./global.css";
+
+// Containers
 import "./container.css";
