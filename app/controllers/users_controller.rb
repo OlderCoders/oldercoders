6095e4859701c2515aeead4b5f7bcfcf21a93ef0
Michael Bester <michael@kimili.com>
2019-05-08 14:25:21 -0400

    Just a bit of cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   include RequestedUser
 
+  before_action :logged_in_user, except: %i[show]
+  before_action :current_account_profile?, except: %i[show]
+
   def show
   end
 
